f1091b1ae4ab95c7dec7310d8583cdaca93e90db
pelonicus <pelonicus@gmail.com>
2026-03-12 11:19:40 -0500

    Actualizo todo

--- backup/run.bat
+++ backup/run.bat
@@ -54,18 +54,18 @@ echo Actualizar fecha
 call actualizar_fecha.pl personal.m3u
 
 echo Backup scripts
-copy *.pl /y "C:\Users\Victor Salvador\Documents\GitHub\mis-archivos\backup"
-copy *.bat /y "C:\Users\Victor Salvador\Documents\GitHub\mis-archivos\backup"
+copy *.pl /y "C:\Users\VictorSalvador\OneDrive - valmersys.com\Documentos\GitHub\mis-archivos\backup"
+copy *.bat /y "C:\Users\VictorSalvador\OneDrive - valmersys.com\Documentos\GitHub\mis-archivos\backup"
 
 echo Upload files
-copy personal.m3u /y "C:\Users\Victor Salvador\Documents\GitHub\mis-archivos"
-copy extra.m3u /y "C:\Users\Victor Salvador\Documents\GitHub\mis-archivos"
-copy gatotv.xml /y "C:\Users\Victor Salvador\Documents\GitHub\mis-archivos"
-copy mitv.xml /y "C:\Users\Victor Salvador\Documents\GitHub\mis-archivos"
-copy epgshare*.xml /y "C:\Users\Victor Salvador\Documents\GitHub\mis-archivos"
+copy personal.m3u /y "C:\Users\VictorSalvador\OneDrive - valmersys.com\Documentos\GitHub\mis-archivos"
+copy extra.m3u /y "C:\Users\VictorSalvador\OneDrive - valmersys.com\Documentos\GitHub\mis-archivos"
+copy gatotv.xml /y "C:\Users\VictorSalvador\OneDrive - valmersys.com\Documentos\GitHub\mis-archivos"
+copy mitv.xml /y "C:\Users\VictorSalvador\OneDrive - valmersys.com\Documentos\GitHub\mis-archivos"
+copy epgshare*.xml /y "C:\Users\VictorSalvador\OneDrive - valmersys.com\Documentos\GitHub\mis-archivos"
 
 C:
-cd "C:\Users\Victor Salvador\Documents\GitHub\mis-archivos"
+cd "C:\Users\VictorSalvador\OneDrive - valmersys.com\Documentos\GitHub\mis-archivos"
 
 git add -A
 git commit -m "Actualizo todo"
